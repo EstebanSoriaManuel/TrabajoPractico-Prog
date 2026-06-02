9cd937cdc1c45e3706d355f22208ef121b55533e
“Soria <clamanusoriar@gmail.com>
2026-06-02 20:53:53 -0300

    se agrega clase coordinadores y el program

---
+++ coordinadores.cs
@@ -0,0 +1,24 @@
+public class Coordinadores : Personas
+{
+    public list<string> areasAsignadas = new List<string>();
+    private List<string> AreasAsignadas {get{return this.areasAsignadas;} set{this.areasAsignadas = value;}}
+
+    public Coordinadores(string nombre, string dni, List<string> areasAsignadas) : base (nombre,dni)
+    {
+        AreasAsignadas = areasAsignadas;
+    }
+^
+
+    public void MostrarInformacion(List<string> areasAsignadas, int voluntariosACargo)
+    {
+        Console.WriteLine("El coordinador tiene asignadas las siguientes áreas:");
+        for (int i = 0 ; i < areasAsignadas.Count ; i++)
+        {
+            Console.WriteLine(areasAsignadas[i]);
+        }
+
+        Console.WriteLine($"El coordinador tiene {voluntariosACargo} voluntarios a cargo.");
+
+    }
+
+}
